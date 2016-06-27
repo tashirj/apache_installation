a0a676c300579d8e1d0ec318bd9971aa3059dbb1
tushar <tushdudhade@gmail.com>
2016-06-27 11:03:55 +0530

    Final one

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,21 +13,24 @@ execute 'openresty_package_download' do
   cwd '/opt/'
   not_if { File.exists?("/opt/ngx_openresty*") }
 end
-
 bash 'extract_openresty' do
-  cwd ::File.dirname(src_filepath)
-  code <<-EOH 
-    tar -xzf #{src_filename} -C #{apache_install_dir}
-    mv #{apache_install_dir}/ngx_openresty-* #{apache_install_dir}/ngx_openresty
+  interpreter "bash"
+  code <<-EOH
+    tar -xzf /opt/ngx_openresty-1.5.8.1.tar.gz -C #{apache_install_dir}
+    rm -rf /opt/ngx_openresty-1.5.8.1.tar.gz
+    mv #{apache_install_dir}/ngx_openresty* #{apache_install_dir}/ngx_openresty
     cd #{apache_install_dir}/ngx_openresty
     ./configure --prefix=/opt/openresty --with-pcre-jit --with-pcre --with-http_ssl_module --with-luajit
     make
     make install
-    PATH=/opt/openresty/nginx/sbin:$PATH
-    export PATH
-    nginx -p `pwd`/ -c conf/nginx.conf
-    ln -s /usr/local/openresty/nginx/conf /etc/nginx
-    nginx -p `pwd`/ -c conf/nginx.conf
+    cd /opt/openresty/nginx/sbin/
+    ./nginx -c ../conf/nginx.conf
+    ln -s /opt/openresty/nginx/conf /etc/nginx
+    ./nginx -c conf/nginx.conf
     EOH
   only_if { ::File.exists?(apache_install_dir) }
 end
+execute 'set-env' do
+	command 'export PATH=$PATH:/opt/openresty/nginx/sbin'
+end
+
